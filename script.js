5565b89b4b99eb51ccd62b868695111b7a0c55fb
valeriacapretti <127955600+valeriacapretti@users.noreply.github.com>
2025-12-18 16:10:06 +0100

    Refactor openSection function for simplicity

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-function openSection(categoryTitle) {
-    // Cambia il testo sul libro
-    const titleElement = document.getElementById('dynamic-title');
-    titleElement.innerText = categoryTitle;
-
-    // Fa scorrere il wrapper per mostrare la sezione dettaglio
+function openSection(title) {
+    document.getElementById('dynamic-title').innerText = title;
     document.body.classList.add('is-viewing-detail');
 }
 
 function closeSection() {
-    // Torna alla schermata principale
     document.body.classList.remove('is-viewing-detail');
 }
