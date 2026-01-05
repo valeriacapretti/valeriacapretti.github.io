a3733cb6cf77868889a33407ecd6e13e770fa17f
valeriacapretti <127955600+valeriacapretti@users.noreply.github.com>
2026-01-05 16:50:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function goToDetail(categoryName) {
-    // Inserisce il testo
-    const bookTitle = document.getElementById('title-on-book');
-    bookTitle.innerText = categoryName;
 
-    // Scorrimento: l'immagine sale rivelando il libro
-    document.getElementById('scroll-container').style.transform = 'translateY(-100vh)';
-}
-
-function goBackHome() {
-    document.getElementById('scroll-container').style.transform = 'translateY(0)';
-}
